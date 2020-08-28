408940c5f765db3db7beb35c4d43661799f1f80e
Andre Aquilau <Andre.Aquilau@outlook.com>
2020-08-28 01:35:56 -0400

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
+import './config/env';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import GlobalStyles from './styles/GlobalStyles';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles></GlobalStyles>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
