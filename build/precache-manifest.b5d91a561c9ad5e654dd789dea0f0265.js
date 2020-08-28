12e0eeec0c00a1df29f92811f6a8e8ea1493bfe4
Andre Aquilau <Andre.Aquilau@outlook.com>
2020-08-28 01:49:27 -0400

    build app

---
+++ build/precache-manifest.b5d91a561c9ad5e654dd789dea0f0265.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dd06bc315b87690f418871f26828e25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fb3aa2612738270c3ed",
+    "url": "/static/js/2.7a592236.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7a592236.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1519058b6aa07c4384e0",
+    "url": "/static/js/main.c1d590c7.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
